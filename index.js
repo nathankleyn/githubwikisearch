1905d52e71713607768f0a328cefc724e46da9cd
Nathan Kleyn <nathan@nathankleyn.com>
2013-10-24 17:47:48 +0100

    Handle errors during process of searching or raising cloning job, merging two together to make cloning get scheduled when a repo is searched for the first time.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var redisClient, jobs,
+var redisClient, jobs, repoExistsInRedis, executeSearch,
+    getMatchingLinesForResults, getMatchingLineForResult, cloneRepo,
     _ = require('underscore'),
     cluster = require('cluster'),
     express = require('express'),
@@ -14,7 +15,7 @@ var redisClient, jobs,
     redisClient = redis.createClient(),
 
     REDIS_PREFIX = 'githubwikisearch-',
-    ALLOWED_USERNAME_REPO_REGEX = /^[a-z0-9]+$/i;
+    ALLOWED_USERNAME_REPO_REGEX = /^[a-z0-9-_]+$/i;
 
 if (cluster.isMaster) {
   var workerCount = 1;
@@ -50,13 +51,12 @@ else {
     });
   });
 
-  /**
-   * Add a repository to the searchable ones.
-   */
-  app.post('/add', function(req, res, next) {
-    // FIXME: Sanitise these strings.
-    var username = req.body.username,
-        repo = req.body.repo;
+  app.get('/search', function(req, res) {
+    var dfd, search,
+        dfds = [],
+        username = req.query['wiki-username'],
+        repo = req.query['wiki-repo'],
+        query = req.query['wiki-q'];
 
     if (username == null || username === "" || !ALLOWED_USERNAME_REPO_REGEX.test(username)) {
       res.send('Invalid username');
@@ -66,84 +66,116 @@ else {
       res.send('Invalid repo name');
     }
 
+    search = reds.createSearch(username + '/' + repo);
+
+    repoExistsInRedis(repo).then(function(exists) {
+      if (exists) {
+        return executeSearch(search, username, repo, query).then(_.partial(getMatchingLinesForResults, username, repo));
+      }
+      else {
+        return cloneRepo(username, repo);
+      }
+    }).then(function(results) {
+      res.render('search', {
+        query: query,
+        results: results,
+        username: username,
+        repo: repo
+      });
+    }).fail(function(err) {
+      console.error(err);
+      next(err);
+    })
+  });
+
+  repoExistsInRedis = function(repo) {
+    var dfd = q.defer();
+
     redisClient.hsetnx(REDIS_PREFIX + 'repos', repo, +new Date(), function(err, wasSet) {
       if (err) {
-        next(err);
+        dfd.reject(err);
         return false;
       }
 
-      if (wasSet) {
-        var job = jobs.create('cloneRepo', {
-          title: 'Clone the "' + username + '/' + repo + '" GitHub repo.',
-          username: username,
-          repo: repo
-        }).save();
-
-        // render success
-        res.send('success');
-      }
-      else {
-        // tell them that repo is already added
-        res.send('repo already added');
-      }
+      dfd.resolve(!wasSet);
     });
-  });
 
-  app.get('/search', function(req, res) {
-    // FIXME: Sanitise these strings.
-    var dfds = [],
-        username = req.query['wiki-username'],
-        repo = req.query['wiki-repo'],
-        query = req.query['wiki-q'],
+    return dfd.promise;
+  };
 
-        search = reds.createSearch(username + '/' + repo);
+  cloneRepo = function(username, repo) {
+    var dfd = q.defer(),
+      job = jobs.create('cloneRepo', {
+        title: 'Clone the "' + username + '/' + repo + '" GitHub repo.',
+        username: username,
+        repo: repo
+      }).save();
+
+    dfd.resolve([]);
+  }
+
+  executeSearch = function(search, username, repo, query) {
+    var dfd = q.defer();
 
     search.query(query).end(function(err, ids) {
       if (err) {
-        next(new Error('Search could not be run, looks to be a problem on our end! Bear with us as we fix it!'));
+        dfd.reject(new Error('Search could not be run, looks to be a problem on our end! Bear with us as we fix it!'));
         return false;
       }
 
-      ids.forEach(function(id, i) {
-        var dfd = q.defer(),
-            parts = id.split(':'),
-            file = parts[0],
-            line = parseInt(parts[1], 10),
-            ext = path.extname(file),
-            pageName = path.basename(file, ext),
-            url = "https://github.com/" + username + '/' + repo + '/wiki';
+      dfd.resolve(ids);
+    });
 
-        if (pageName === '_Sidebar' || pageName === '_Footer' || pageName === '_Header') {
-          return;
-        }
+    return dfd.promise;
+  };
 
-        if (pageName !== 'Home') {
-          url += '/' + pageName;
-        }
+  getMatchingLinesForResults = function(username, repo, ids) {
+    var dfds = ids.reduce(function(acc, id) {
+      var dfd = getMatchingLineForResult(username, repo, id);
 
-        dfds.push(dfd.promise);
+      if (dfd) {
+        acc.push(dfd);
+      }
 
-        lazy(fs.createReadStream(file)).lines.skip(line - 1).take(1).map(function(line) {
-          return line.toString();
-        }).join(function(lines) {
-          dfd.resolve({
-            url: url,
-            title: pageName.replace(/-/g, ' '),
-            content: lines.join('\n')
-          });
-        });
-      }, []);
+      return acc;
+    }, []);
 
-      q.all(dfds).then(function(results) {
-        res.render('search', {
-          query: query,
-          results: results,
-          username: username,
-          repo: repo
+    return q.all(dfds);
+  };
+
+  getMatchingLineForResult = function(username, repo, id) {
+    var dfd = q.defer(),
+        parts = id.split(':'),
+        file = parts[0],
+        line = parseInt(parts[1], 10),
+        pageName = path.basename(file, path.extname(file)),
+        url = "https://github.com/" + username + '/' + repo + '/wiki';
+
+    if (pageName === '_Sidebar' || pageName === '_Footer' || pageName === '_Header') {
+      return;
+    }
+
+    if (pageName !== 'Home') {
+      url += '/' + pageName;
+    }
+
+    lazy(fs.createReadStream(file))
+      .lines
+      .skip(line - 1)
+      .take(1)
+      .map(function(line) {
+        return line.toString();
+      })
+      .join(function(lines) {
+        dfd.resolve({
+          url: url,
+          title: pageName.replace(/-/g, ' '),
+          content: lines.join('\n')
         });
       });
-    });
-  });
+
+    return dfd.promise;
+  };
 
   console.log('Worker ' + cluster.worker.id + ' is listening on port 3000.');
   app.listen(3000);
